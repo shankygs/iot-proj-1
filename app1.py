833838139b140de3dc794d047765d9a42fea97f4
shanky1171 <shankar.gowri@wipro.com>
2018-10-14 01:10:21 +0530

    Added Database connectivity-1

--- app1.py
+++ app1.py
@@ -1,6 +1,23 @@
 from flask import Flask, redirect, url_for, request, render_template
+import os
+
+from models import *
 
 app = Flask(__name__)
+
+POSTGRES = {
+    'user': 'Shruthi',
+    'pw': 'root123',
+    'db': 'demo',
+    'host': 'localhost',
+    'port': '5432',
+}
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+db.init_app(app)
+db.create_all()
 
 @app.route('/')
 def index():
